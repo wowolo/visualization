4585a50b8e52a299febf83f8bbd9688a4b9c5879
Sebastian <sebastian.schein@yahoo.com>
2022-07-20 22:10:42 +0200

    update experiments for running 'euler_test2' on Euler cluster

--- bash_files/main_func.sh
+++ bash_files/main_func.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo $project_path
-python "$project_path/smain.py" --experimentbatch_name $tag --config_trainer ${config_trainer[@]} --num_config \$LSB_JOBINDEX
+python $project_path/main.py --experimentbatch_name $tag --config_trainer ${config_trainer[@]} --num_config "\$LSB_JOBINDEX"
